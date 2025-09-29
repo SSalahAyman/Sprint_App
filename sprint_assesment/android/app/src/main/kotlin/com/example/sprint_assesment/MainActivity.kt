e6dd3a8e64c5ec6aeb5f10e7439c065547aed2d5
SSalahAyman <salahayman488@gmail.com>
2025-09-30 02:31:20 +0300

    project  finished

---
+++ sprint_assesment/android/app/src/main/kotlin/com/example/sprint_assesment/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sprint_assesment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ sprint_assesment/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:sprint_assesment/presentation/screens/splash_screen.dart';
+import 'package:sprints_assesment/presentation/screens/splash_screen.dart';
+import 'package:sprints_assesment/presentation/screens/welcome_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ShopCraft',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        fontFamily: 'Suwannaphum',
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
